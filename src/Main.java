26a6273fe97701f854b7f6d66dc7e06f6fe58d80
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-06-21 16:07:17 +0900

    List<Employee>に対するStream処理に変更

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,47 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Main {
     public static void main(String[] args) {
-        List<String> employees1 = List.of("ichiro", "jiro", "saburo", "siro", "goro", "ichiro");
-        List<String> employees2 = employees1.stream().distinct().toList();
-        System.out.println("ListA = " + employees1);
-        System.out.println("ListB = " + employees2);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("ichiro"));
+        employees.add(new Employee("jiro"));
+        employees.add(new Employee("saburo"));
+        employees.add(new Employee("siro"));
+        employees.add(new Employee("goro"));
+        employees.add(new Employee("ichiro"));
 
 
-        List<String> sortedResult = employees2.stream().sorted().toList();
-        System.out.println("昇順:" + sortedResult);
+        System.out.println("重複を削除");
+        List<String> employees1 = employees.stream()
+                .map(Employee::getName)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(employees1);
 
 
-        long count = employees2.stream().filter(name -> name.startsWith("s")).count();
+        System.out.println("昇順");
+        List<String> sortedResult = employees1.stream().sorted().toList();
+        System.out.println(sortedResult);
+
+
+        long count = employees1.stream()
+                .filter(name -> name.startsWith("s"))
+                .count();
         System.out.println("\"s\" の個数は" + count);
 
 
-        boolean hasIchiro = employees2.stream().anyMatch(name -> name.equals("ichiro"));
+        System.out.println("リストに\"ichiro\"が存在するか");
+        boolean hasIchiro = employees1.stream()
+                .anyMatch(name -> name.equals("ichiro"));
         System.out.println(hasIchiro);
 
 
-        sortedResult.stream().map(String::toUpperCase).forEach(System.out::println);
-
+        sortedResult.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
 
     }
 }
